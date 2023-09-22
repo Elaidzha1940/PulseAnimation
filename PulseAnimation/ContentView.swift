331cd6ad20384a506b7cb13f172229f92879479d
Elaidzha1940 <eli080293@gmail.com>
2023-09-22 16:16:16 +0300

    PulseAnimation

---
+++ PulseAnimation/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: PulseAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  DAte: 22.09.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PulseAnimation/PulseAnimationApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: PulseAnimation
+//  File: PulseAnimationApp.swift
+//  Created by: Elaidzha Shchukin
+//  DAte: 22.09.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct PulseAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
